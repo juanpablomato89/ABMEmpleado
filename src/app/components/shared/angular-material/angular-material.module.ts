8eec50ef32118cf8eca4526d2fb3085261adebc7
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-08-04 21:16:59 -0400

    Add Tabla de Material

--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -4,14 +4,28 @@ import { CommonModule } from '@angular/common';
 //Material Modules
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSliderModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
 
-  ], exports: [MatSliderModule, MatToolbarModule]
+  ], exports: [
+    MatSliderModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
 })
 export class AngularMaterialModule { }
