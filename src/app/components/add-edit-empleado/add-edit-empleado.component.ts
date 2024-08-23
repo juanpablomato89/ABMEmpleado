3e29d1ac5f356e0ce59c8f7aad150b6ea7451048
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-08-22 22:02:18 -0400

    crear objetos a partir de los datos insetados en un formulario

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-add-edit-empleado',
@@ -14,7 +16,7 @@ import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 export class AddEditEmpleadoComponent implements OnInit {
   estadoCiviles: any[] = ['Soltero', 'Casado', 'Divorciado'];
   myForm: FormGroup;
-  constructor(private builder: FormBuilder) {
+  constructor(private builder: FormBuilder, private empleadoService: EmpleadoService) {
     this.myForm = builder.group({
       nombre: [''],
       correo: [''],
@@ -26,6 +28,18 @@ export class AddEditEmpleadoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  guardarEmpleado() {
+    const empleado: Empleado = {
+      nombreCompleto: this.myForm.get('nombre')?.value,
+      correo: this.myForm.get('correo')?.value,
+      fechaIngreso: this.myForm.get('fechaIngreso')?.value,
+      telefono: this.myForm.get('telefono')?.value,
+      estadoCivil: this.myForm.get('estadoCivil')?.value,
+      sexo: this.myForm.get('sexo')?.value,
+    }
+    this.empleadoService.agregarEmpleado(empleado);
   }
 
 }
